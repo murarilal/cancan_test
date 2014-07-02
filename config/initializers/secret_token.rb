52187610b9baeeb51893922dc77877fef7947d17
murari <murari@ubuntu.(none)>
2014-07-02 23:40:07 +0530

    cancan initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CancanTest::Application.config.secret_key_base = '619a3d812ea849b2d297fdf21790849843bc184256d9537432c8bc10eddc5743f72cfe48236b71c3ea8f71da282ec5f6be358eb0d8244ede37413a2ddc5be858'
